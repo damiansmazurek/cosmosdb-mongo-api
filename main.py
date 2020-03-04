06c8452a3372b7c952392c844f8d79a258378dd5
Damian <damian.s.mazurek@gmail.com>
2020-03-04 01:38:04 +0100

    bug fixing

--- main.py
+++ main.py
@@ -8,18 +8,15 @@ from time import sleep
 CONNECTION_STRING = os.environ.get('CONNECTION_STRING')
 DB_NAME = os.environ.get('DB_NAME')
 COLLECTION_NAME = os.environ.get('COLLECTION_NAME')
-BATCH_SIZE = os.environ.get('BATCH_SIZE')
+BATCH_SIZE = int(os.environ.get('BATCH_SIZE'))
 DEVICE_ID = os.environ.get('DEVICE_ID')
 
 # Create client
 myclient = pymongo.MongoClient(CONNECTION_STRING)
 mydb = myclient[DB_NAME]
 mycol = mydb[COLLECTION_NAME]
-
-batchdata= []
-while true:
+while True:
     for i in range(BATCH_SIZE):
-    seed(datetime.now().timestamp())
-    batchdata.append({ "deviceid": DEVICE_ID, "timestamp": datetime.now() , a:random()*100, b: random()*200, c: random()*50, message: 'Telemetric entity'})
-    x = mycol.insert_many(mydict)
+        seed(datetime.now().timestamp())
+        print(mycol.insert_one({ "deviceid": DEVICE_ID, "timestamp": datetime.now() , "a":random()*100, "b": random()*200, "c": random()*50, "message": "Telemetric entity"}))
     sleep(1)
