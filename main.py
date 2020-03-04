cfb6c8867dd529f6148a6d74ae34157b37cad97d
Damian <damian.s.mazurek@gmail.com>
2020-03-04 01:18:49 +0100

    Initialize software

---
+++ main.py
@@ -0,0 +1,23 @@
+import pymongo
+import os
+from random import seed, random
+from datetime import datetime
+
+# Get env variables
+CONNECTION_STRING = os.environ.get('CONNECTION_STRING')
+DB_NAME = os.environ.get('DB_NAME')
+COLLECTION_NAME = os.environ.get('COLLECTION_NAME')
+BATCH_SIZE = os.environ.get('BATCH_SIZE')
+DEVICE_ID = os.environ.get('DEVICE_ID')
+
+# Create client
+myclient = pymongo.MongoClient(CONNECTION_STRING)
+mydb = myclient[DB_NAME]
+mycol = mydb[COLLECTION_NAME]
+
+batchdata= []
+for i in range(BATCH_SIZE):
+    seed(datetime.now().timestamp())
+    batchdata.append({ "deviceid": DEVICE_ID, "timestamp": datetime.now() , a:random()*100, b: random()*200, c: random()*50, message: 'Telemetric entity'})
+
+x = mycol.insert_many(mydict)
